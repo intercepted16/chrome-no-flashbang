8211e093ce9fd6b959a60d551c5202b18f22f249
intercepted16 <162676516+intercepted16@users.noreply.github.com>
2024-05-19 19:34:37 +1000

    Add a check for all possible Chrome locations & check if Chrome is installed, a little refactoring.

--- main.py
+++ main.py
@@ -1,21 +1,38 @@
-# launch chrome offscreen and tiny to avoid flash
 import subprocess
-import pygetwindow as gw  # Import pygetwindow
+import pygetwindow as gw
+import os
 
+path = None
+possible_locations = [
+    f"{os.environ('LOCALAPPDATA')}",
+    f"{os.environ('ProgramFiles')}",
+    f"{os.environ('ProgramFiles(x86)')}",
+]
+
+for location in possible_locations:
+    if os.path.exists(f"{location}\\Google\\Chrome\\Application\\chrome.exe"):
+        path = f"{location}\\Google\\Chrome\\Application\\chrome.exe"
+        break
+
+if not path:
+    print("Chrome not found")
+    exit()
+
+# launch chrome offscreen and tiny to avoid flash
 subprocess.Popen(
     [
-        "C:\Program Files\Google\Chrome\Application\chrome.exe",
+        path,
         "--window-position=-3940,2160",
         "--window-size=0,0",
     ],
     creationflags=subprocess.CREATE_NEW_CONSOLE,
 ).pid
 
-win = None
-
+# wait till window is opened
 while True:
     if gw.getWindowsWithTitle("Chrome"):
         win = gw.getWindowsWithTitle("Chrome")[0]
+        # maximize window
         win.moveTo(0, 0)
         win.size = (3800, 2120)
         exit()
